8400065c6d338a30e419eb7b8468b470d5c793c9
Victor Zuluaga <victor@getmetatate.com>
2026-06-23 01:56:40 -0500

    Added theming tag to the initial main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import ResponsiveDrawer from './Sidebar.tsx'
 import './App.css'
+import { ThemeProvider } from '@mui/material/styles'
+import { theme } from './theme.tsx'
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <ResponsiveDrawer />
-    </>
+    </ThemeProvider>
   )
 }
 
